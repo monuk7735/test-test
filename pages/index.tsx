9b9a37a863ea98bc1e4afecdee860b4cc586ea50
Monu Kumar <monuk7735@gmail.com>
2025-04-07 21:05:36 +0530

    gh-page change ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -90,7 +90,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          Testing
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
